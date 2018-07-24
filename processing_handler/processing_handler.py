04140a8a8631162d6c25bf755ca019fbc58711bc
= <=>
2018-07-24 17:02:30 -0400

    Added python processing handler

---
+++ processing_handler/processing_handler.py
@@ -0,0 +1,19 @@
+import subprocess
+
+class P5:
+    def __init__(self):
+        pass
+    def run_sketch(self, sketchname):
+        path = f"java -jar .\processingpy\processing-py.jar  .\processing_sketches\{sketchname}.py"
+        try:
+            res = subprocess.check_output(path.split())
+            return res
+        except OSError as e:
+            return e
+        except subprocess.CalledProcessError as e:
+            return e
+
+
+if __name__ == "__main__":
+    run = P5()
+    run.run_sketch("mouse")
---
+++ processing_handler/processing_sketches/mouse.py
@@ -0,0 +1,9 @@
+def setup():
+    size(400, 400)
+
+def draw():
+    background(255)
+    fill(0)
+    ellipse(mouseX, mouseY, 50, 50)
+    delay(5000)
+    exit()
