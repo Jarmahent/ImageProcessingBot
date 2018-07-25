a40f535a811450e3265e0a16ff1ea7955065373d
Kevin <kjh7796@gmail.com>
2018-07-24 22:56:09 -0400

    Changing processing sketches

--- processing_handler/processing_sketches/mouse.py
+++ processing_handler/processing_sketches/mouse.py
@@ -1,10 +1,24 @@
-def setup(): #Loads an image onto the canvas.
-    img = PImage()
 
-    size(400, 400)
+def setup():
+    global img
+    img = loadImage("../media/preprocessed/unprocessed.jpg")
+    this.surface.setSize(img.width, img.height)
+    global output
+    output = createWriter("positions.txt")
+
 
 def draw():
-    img = loadImage("carbon.jpg");
-    image(img, 0, 0);
-    fill(0)
-    ellipse(mouseX, mouseY, 50, 50)
+    colorMode(HSB, 360, 100, 100)
+    for y in range(0, img.height, 10):
+        for x in range(0, img.width, 10):
+            output.println(x)
+            x += 10
+            c = img.get(x, y)
+            fill(c)
+            noStroke()
+            ellipse(x, y, 1, 1)
+
+
+
+    save("processed.png")
+    noLoop()
